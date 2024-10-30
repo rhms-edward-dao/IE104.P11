66bddab8ec2dbbfe4c26bb2468f5d3c7e82a1adb
NguyenKiet-Anh <anhkiet.nguyen798@gmail.com>
2024-10-30 10:50:24 +0700

    Fix model for backend

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = (
-    "postgresql://postgres:027735@localhost:5432/inventory_management"
+    "postgresql://postgres:027735@localhost:5432/IE104.P11"
 )
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
