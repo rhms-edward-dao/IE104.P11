f4cfb83d42be069788662bfdd114af21a56e2174
NguyenKiet-Anh <anhkiet.nguyen798@gmail.com>
2024-10-31 10:40:27 +0700

    Complete fixing all feature for QUAN, MATHANG & LOAIMATHANG, DAILY & LOAIDAILY

--- frontend/src/pages/ProductCategory/ProductsEditPage.jsx
+++ frontend/src/pages/ProductCategory/ProductsEditPage.jsx
@@ -33,7 +33,7 @@ const ProductAdminEditPage = () => {
   // // For editing product
   const [currentProductName, setCurrentProductName] = useState("");
 
-  const [currentUnitPrice, setCurrentUnitPrice] = useState("");
+  const [currentUnitPrice, setCurrentUnitPrice] = useState(0);
   const [currentStockQuantity, setCurrentStockQuantity] = useState("");
   const [currentUnit, setCurrentUnit] = useState("");
 
@@ -66,7 +66,7 @@ const ProductAdminEditPage = () => {
       setCurrentStoreName(currentProduct.tendaily);
 
       if (currentProduct.Mathang.hinhanh !== null) {
-        setImageForShow(currentProduct.Mathang.hinhanh);
+        setCurrentImage(currentProduct.Mathang.hinhanh);
       }
 
       // Get all existed product category name
@@ -81,13 +81,14 @@ const ProductAdminEditPage = () => {
   }, []);
 
   // Functions here
-  // // For updating image
+  // For updating image
   const handleUploadImage = (e) => {
-    setImageForShow(e.target.files[0]);
-    setCurrentImage(URL.createObjectURL(e.target.files[0]));
+    setCurrentImage(e.target.files[0]);
+    setImageForShow(URL.createObjectURL(e.target.files[0]));
   };
   // // For editing current product
   const updateData = async (
+    mamathang,
     tenmathang,
     tenloaimathang,
     tendaily,
@@ -96,14 +97,11 @@ const ProductAdminEditPage = () => {
     tendvt,
     hinhanh
   ) => {
+    
     let check_tenmathang = true;
     let check_dongia = true;
     let check_soluongton = true;
     let check_tendvt = true;
-    let check_hinhanh = true;
-
-    // Constraints for checking format
-    const isOnlyDigit = (input) => /^\d+$/.test(input);
 
     // Checking before calling add api
     // // Check tenmathang
@@ -118,16 +116,10 @@ const ProductAdminEditPage = () => {
     if (dongia < 1000) {
       alert("Đơn giá tối thiểu là 1000 đồng");
       check_dongia = false;
-    } else if (!isOnlyDigit(dongia)) {
-      alert("Đơn giá chỉ được có ký tự chữ số");
-      check_dongia = false;
-    }
+    } 
     // // Check soluongton
     if (soluongton < 0) {
       alert("Số lượng tồn không âm");
-      check_soluongton = false;
-    } else if (!isOnlyDigit(soluongton)) {
-      alert("Số lượng tồn chỉ được có ký tự chữ số");
       check_soluongton = false;
     }
     // // Check tendvt
@@ -138,22 +130,16 @@ const ProductAdminEditPage = () => {
       alert("Tên đơn vị tính quá dài");
       check_tendvt = false;
     }
-    // // Check hinhanh
-    if (hinhanh === undefined) {
-      alert("Chưa chọn hình ảnh");
-      check_hinhanh = false;
-    }
-
     if (
       check_tenmathang &&
       check_dongia &&
       check_soluongton &&
-      check_tendvt &&
-      check_hinhanh
-    ) {
-      if (currentImage === undefined) {
+      check_tendvt
+    ) {      
+      if (imageForShow === undefined) {
         hinhanh = null;
       }
+
       let item = {
         tenmathang: tenmathang,
         tenloaimathang: tenloaimathang,
@@ -162,8 +148,8 @@ const ProductAdminEditPage = () => {
         soluongton: soluongton,
         tendvt: tendvt,
         hinhanh: hinhanh,
-      };
-      const data = await updateProduct(item);
+      };        
+      const data = await updateProduct(mamathang, item);
       if (data.message === "Cập nhật mặt hàng thất bại") {
         alert("Cập nhật mặt hàng thất bại");
       } else if (data.message === "Tên mặt hàng đã tồn tại") {
@@ -174,7 +160,6 @@ const ProductAdminEditPage = () => {
       }
     }
   };
-
   // Return render here
   return (
     <div>
@@ -229,8 +214,7 @@ const ProductAdminEditPage = () => {
               id="product-name-edit"
               name="product-name-edit"
               type="text"
-              placeholder={currentProductName}
-              values={currentProductName}
+              value={currentProductName}
               onChange={(e) => setCurrentProductName(e.target.value)}
               required
             />
@@ -248,8 +232,7 @@ const ProductAdminEditPage = () => {
               id="unit-price-edit"
               name="unit-price-edit"
               type="number"
-              placeholder={currentUnitPrice}
-              values={currentUnitPrice}
+              value={currentUnitPrice}
               onChange={(e) => setCurrentUnitPrice(e.target.value)}
               required
             />
@@ -267,8 +250,7 @@ const ProductAdminEditPage = () => {
               id="stock-quantity-edit"
               name="stock-quantity-edit"
               type="number"
-              placeholder={currentStockQuantity}
-              values={currentStockQuantity}
+              value={currentStockQuantity}
               onChange={(e) => setCurrentStockQuantity(e.target.value)}
               required
             />
@@ -286,8 +268,7 @@ const ProductAdminEditPage = () => {
               id="unit-edit"
               name="unit-edit"
               type="text"
-              placeholder={currentUnit}
-              values={currentUnit}
+              value={currentUnit}
               onChange={(e) => setCurrentUnit(e.target.value)}
               required
             />
@@ -360,9 +341,9 @@ const ProductAdminEditPage = () => {
             <img
               width={"250px"}
               src={
-                currentImage !== undefined
-                  ? currentImage
-                  : `data:image/jpeg;base64, ${imageForShow}`
+                imageForShow
+                  ? imageForShow
+                  : `data:image/jpeg;base64, ${currentImage}`
               }
               alt="Hình ảnh mặt hàng"
               className="rounded text-black transition-colors duration-300 dark:text-white"
@@ -375,319 +356,3 @@ const ProductAdminEditPage = () => {
 };
 
 export default ProductAdminEditPage;
-
-//
-//
-// -------------------------------------------- OLD VERSION BACKUP --------------------------------------------
-//
-// import React, { useEffect, useState } from "react";
-// import { NavLink, useNavigate, useParams } from "react-router-dom";
-
-// import ReturnIcon from "../../images/icons/return-button.png";
-// import Header from "../../components/Header";
-
-// import { updateProduct, getProductById } from "../../assets/AdminProduct";
-// import { getAllCategoryName } from "../../assets/TypeOfProduct";
-// import { getAllStoreName } from "../../assets/Stores/StoreData";
-
-// const ProductAdminAddPage = () => {
-//   // Variables here
-//   const [productName, setProductName] = useState("");
-//   const [productPrice, setProductPrice] = useState(0);
-//   const [productAmount, setProductAmount] = useState(0);
-//   const [productUnitName, setProductUnitName] = useState("");
-
-//   const [catData, setCatData] = useState([]);
-//   const [catName, setCatName] = useState("");
-
-//   const [storeData, setStoreData] = useState([]);
-//   const [storeName, setStoreName] = useState("");
-
-//   const [image, setImage] = useState();
-//   const [imageData, setImageData] = useState();
-
-//   const nagivate = useNavigate();
-//   const { productId } = useParams();
-
-//   // Functions here
-//   useEffect(() => {
-//     const fetchAllData = async (id) => {
-//       const existedCatData = await getAllCategoryName();
-//       setCatData(existedCatData);
-
-//       const existedStoreData = await getAllStoreName();
-//       setStoreData(existedStoreData);
-
-//       // Set existed data to other stuff
-//       const existedData = await getProductById(id);
-
-//       setCatName(existedData.tenloaimathang);
-//       setStoreName(existedData.tendaily);
-//       setProductName(existedData.Mathang.tenmathang);
-//       setProductPrice(existedData.Mathang.dongia);
-//       setProductAmount(existedData.Mathang.soluongton);
-//       setProductUnitName(existedData.Mathang.tendvt);
-
-//       if (existedData.Mathang.hinhanh !== null) {
-//         setImageData(existedData.Mathang.hinhanh);
-//       }
-//     };
-//     fetchAllData(productId);
-//   }, []);
-//   const handleUploadImage = (e) => {
-//     setImageData(e.target.files[0]);
-//     setImage(URL.createObjectURL(e.target.files[0]));
-//   };
-
-//   const updateData = async (
-//     mamathang,
-//     tenmathang,
-//     dongia,
-//     soluongton,
-//     tendvt,
-//     tenloaimathang,
-//     tendaily,
-//     hinhanh
-//   ) => {
-//     // Checking before adding
-//     let check_tenmathang = true;
-//     let check_tendvt = true;
-//     let check_hinhanh = true;
-
-//     if (tenmathang.length < 1) {
-//       alert("Tên mặt hàng không được rỗng");
-//       check_tenmathang = false;
-//     } else if (tenmathang.length > 100) {
-//       alert("Tên mặt hàng quá dài");
-//       check_tenmathang = false;
-//     }
-
-//     if (tendvt.length < 1) {
-//       alert("Tên đơn vị tính không được rỗng");
-//       check_tendvt = false;
-//     } else if (tendvt.length > 50) {
-//       alert("Tên đơn vị tính quá dài");
-//       check_tendvt = false;
-//     }
-
-//     if (hinhanh === undefined) {
-//       alert("Chưa chọn hình ảnh");
-//       check_hinhanh = false;
-//     }
-
-//     if (check_tenmathang && check_tendvt && check_hinhanh) {
-//       if (image === undefined) {
-//         hinhanh = null;
-//       }
-//       let item = {
-//         tenmathang: tenmathang,
-//         dongia: dongia,
-//         soluongton: soluongton,
-//         tendvt: tendvt,
-//         tenloaimathang: tenloaimathang,
-//         tendaily: tendaily,
-//         hinhanh: hinhanh,
-//       };
-//       const data = await updateProduct(mamathang, item);
-//       if (data.message === "Cập nhật mặt hàng thành công") {
-//         alert("Cập nhật mặt hàng thành công");
-//         nagivate("/product-categorys");
-//       } else if (data.message === "Tên mặt hàng đã tồn tại") {
-//         alert("Tên mặt hàng đã tồn tại");
-//       } else {
-//         alert("Cập nhật mặt hàng thất bại");
-//       }
-//     }
-//   };
-//   // Return here
-//   return (
-//     <div>
-//       <div>
-//         <Header></Header>
-//       </div>
-//       <hr />
-
-//       <div>
-//         <div>
-//           <div className="flex items-center gap-40">
-//             <NavLink to={"/product-categorys"}>
-//               <button>
-//                 <img
-//                   src={ReturnIcon}
-//                   alt="Icon trở lại"
-//                   className="w-15 h-12"
-//                 />
-//               </button>
-//             </NavLink>
-//           </div>
-//           <div className="flex mt-5">
-//             <div className="w-1/2">
-//               <p className="text-xl font-bold italic">{"Cập nhật mặt hàng"}</p>
-//             </div>
-//             <div className="w-1/2 flex justify-end mr-5">
-//               <button
-//                 className="px-2 py-3 bg-red-500 rounded rounded-xl"
-//                 onClick={() =>
-//                   updateData(
-//                     productId,
-//                     productName,
-//                     productPrice,
-//                     productAmount,
-//                     productUnitName,
-//                     catName,
-//                     storeName,
-//                     imageData
-//                   )
-//                 }
-//               >
-//                 <p className="font-bold text-white text-lg">Cập nhật</p>
-//               </button>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//       <div className="m-5">
-//         <div className="block space-y-8">
-//           <div className="space-y-4">
-//             <label htmlFor="product-name-add" className="font-bold text-lg">
-//               Tên mặt hàng
-//             </label>
-//             <br />
-//             <input
-//               id="product-name-add"
-//               name="product-name-add"
-//               type="text"
-//               className="w-full py-2 text-lg border border-black rounded-lg"
-//               placeholder="   Tên mặt hàng..."
-//               value={productName}
-//               onChange={(e) => setProductName(e.target.value)}
-//               required
-//             />
-//           </div>
-
-//           <div className="space-y-4">
-//             <label htmlFor="product-price-add" className="font-bold text-lg">
-//               Đơn giá
-//             </label>
-//             <br />
-//             <input
-//               id="product-price-add"
-//               name="product-price-add"
-//               type="number"
-//               className="w-full py-2 text-lg border border-black rounded-lg"
-//               placeholder="   Đơn giá..."
-//               value={productPrice}
-//               onChange={(e) => setProductPrice(e.target.value)}
-//               required
-//             />
-//           </div>
-
-//           <div className="space-y-4">
-//             <label htmlFor="product-amount-add" className="font-bold text-lg">
-//               Số lượng tồn
-//             </label>
-//             <br />
-//             <input
-//               id="product-amount-add"
-//               name="product-amount-add"
-//               type="number"
-//               className="w-full py-2 text-lg border border-black rounded-lg"
-//               placeholder="   Số lượng tồn..."
-//               value={productAmount}
-//               onChange={(e) => setProductAmount(e.target.value)}
-//               required
-//             />
-//           </div>
-
-//           <div className="space-y-4">
-//             <label
-//               htmlFor="product-unit-name-add"
-//               className="font-bold text-lg"
-//             >
-//               Tên đơn vị tính
-//             </label>
-//             <br />
-//             <input
-//               id="product-unit-name-add"
-//               name="product-unit-name-add"
-//               type="text"
-//               className="w-full py-2 text-lg border border-black rounded-lg"
-//               placeholder="   Tên đơn vị tính..."
-//               value={productUnitName}
-//               onChange={(e) => setProductUnitName(e.target.value)}
-//               required
-//             />
-//           </div>
-
-//           <div className="space-y-4">
-//             <label htmlFor="cat-name-add" className="font-bold text-lg">
-//               Tên loại mặt hàng
-//             </label>
-//             <br />
-//             <select
-//               id="cat-name-add"
-//               name="cat-name-add"
-//               type="text"
-//               className="px-5 py-3 rounded bg-stone-300"
-//               value={catName}
-//               onChange={(e) => setCatName(e.target.value)}
-//             >
-//               {catData.map((item) => (
-//                 <option key={item} value={item}>
-//                   {item}
-//                 </option>
-//               ))}
-//             </select>
-//           </div>
-
-//           <div className="space-y-4">
-//             <label htmlFor="store-name-add" className="font-bold text-lg">
-//               Tên đại lý
-//             </label>
-//             <br />
-//             <select
-//               id="store-name-add"
-//               name="store-name-add"
-//               type="text"
-//               className="px-5 py-3 rounded bg-stone-300"
-//               value={storeName}
-//               onChange={(e) => setStoreName(e.target.value)}
-//             >
-//               {storeData.map((item) => (
-//                 <option key={item} value={item}>
-//                   {item}
-//                 </option>
-//               ))}
-//             </select>
-//           </div>
-
-//           <div className="space-y-4">
-//             <label htmlFor="product-image-edit" className="font-bold text-lg">
-//               Hình mặt hàng
-//             </label>
-//             <br />
-//             <input
-//               id="product-image-edit"
-//               name="product-image-edit"
-//               type="file"
-//               accept="image/*"
-//               onChange={handleUploadImage}
-//             />
-//             {image !== undefined ? (
-//               <img src={image} alt="Hình ảnh mặt hàng" />
-//             ) : (
-//               <img
-//                 width="250px"
-//                 src={`data:image/jpeg;base64, ${imageData}`}
-//                 alt="Hình ảnh mặt hàng"
-//                 className="rounded"
-//               />
-//             )}
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default ProductAdminAddPage;
