aa35ce281a490205ee576f6742b501d3f75f15dc
blackheartHN <nguyentran55it@gmail.com>
2024-11-05 01:43:08 +0700

    Uploaded revenue icon and AdminReport folder for report function, with 1 main page and 3 placeholder pages

---
+++ frontend/src/pages/AdminReport/ProfitReport.jsx
@@ -0,0 +1,16 @@
+// src/pages/Report/ProfitReport.jsx
+import React from 'react';
+import Header from '../../components/Header';
+
+function ProfitReport() {
+  return (
+    <div>
+      <Header headerTitle="Báo Cáo Lợi Nhuận" />
+      <div style={{ padding: '20px', textAlign: 'center' }}>
+        <h2>Placeholder</h2>
+      </div>
+    </div>
+  );
+}
+
+export default ProfitReport;
