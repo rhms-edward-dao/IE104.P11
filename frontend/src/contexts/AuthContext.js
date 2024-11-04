33d5c6aa7971300a0a71249a4a10e8c17aa0dc1e
rhms-edward-dao <edwarddao20@gmail.com>
2024-11-04 22:28:18 +0700

    Upload Icons for DataCard and Update StaffData + AuthContext

--- frontend/src/contexts/AuthContext.js
+++ frontend/src/contexts/AuthContext.js
@@ -2,44 +2,44 @@ import { createContext, useContext, useState } from "react";
 
 const AuthContext = createContext();
 
-export const AuthProvider = ({children}) => {
-    // Variables here
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const [isAdmin, setIsAdmin] = useState(false);
-    const [userInfo, setUserInfo] = useState({});
+export const AuthProvider = ({ children }) => {
+  // Variables here
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [userInfo, setUserInfo] = useState({});
 
-    // Functions here
-    const login = (userData) => {
-        setIsLoggedIn(true);
-        setUserInfo(userData);
-    };
+  // Functions here
+  const login = (userData) => {
+    setIsLoggedIn(true);
+    setUserInfo(userData);
+  };
 
-    const logout = () => {
-        setIsLoggedIn(false);
-        setIsAdmin(false);
-        setUserInfo({});
-    };
+  const logout = () => {
+    setIsLoggedIn(false);
+    setIsAdmin(false);
+    setUserInfo({});
+  };
 
-    const isAdminYes = () => {
-        setIsAdmin(true);
-    };
+  const isAdminYes = () => {
+    setIsAdmin(true);
+  };
 
-    // Return here
-    return (
-        <AuthContext.Provider
-            value={{
-                isLoggedIn,
-                isAdmin,
-                userInfo,
-                login,
-                logout,
-                isAdminYes,
-                setUserInfo
-            }}
-        >
-            {children}
-        </AuthContext.Provider>
-    );
+  // Return here
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn,
+        isAdmin,
+        userInfo,
+        login,
+        logout,
+        isAdminYes,
+        setUserInfo,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export const useAuth = () => useContext(AuthContext);
