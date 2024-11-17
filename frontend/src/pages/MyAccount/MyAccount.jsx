8ccae79512098e8652673a3b819b24fd24649bb9
NguyenKiet-Anh <anhkiet.nguyen798@gmail.com>
2024-11-17 12:28:25 +0700

    Complete building myaccount page

--- frontend/src/pages/MyAccount/MyAccount.jsx
+++ frontend/src/pages/MyAccount/MyAccount.jsx
@@ -28,6 +28,7 @@ function MyAccount() {
   const pass = "No String";
   const { userInfo } = useAuth();
   const [accountData, setAccountData] = useState({});
+  const [loading, setLoading] = useState(true);
 
   // Use Effect here
   // // For getting current loged account infomation
@@ -38,6 +39,7 @@ function MyAccount() {
         if (accoutInfo.length === 0) {
           setAccountData([]);
         } else {
+          setLoading(false);
           setAccountData(accoutInfo);
         }
       } catch (error) {
@@ -48,149 +50,145 @@ function MyAccount() {
   }, []);
   // Return render here
   return (
-    <div>
-      <Header headerTitle={Title.Account}></Header>
-      <div className="my-8 mx-4 grid grid-cols-2 bg-white dark:bg-[#363636] transition-colors duration-300 rounded-lg shadow-lg">
-        <div className="border-r-2 border-gray-200 p-4 pr-6">
-          <div className="flex justify-between items-center">
-            <h1 className="m-2 text-2xl font-bold text-black dark:text-white transition-colors duration-300">
-              {Section1.Title}
-            </h1>
-            <NavLink
-              className="flex items-center gap-2 rounded-lg bg-green-500 px-4 py-2 font-bold text-white"
-              to="my-account-edit-page"
-            >
-              <p className="hidden sm:hidden md:hidden lg:inline-block">
-                {Edit}
-              </p>
-              <img src={EditIcon} alt="Icon chỉnh sửa" />
-            </NavLink>
-          </div>
-          <div className="mx-2 my-8 flex items-center">
-            <h3 className="w-1/4 text-xl font-semibold text-black dark:text-white transition-colors duration-300">
-              {Section1.Label1}:
-            </h3>
-            <div className="relative w-3/4 flex justify-center">
-              <div className="w-32 h-32 rounded-full bg-gray-300 dark:bg-gray-600 flex items-center justify-center transition-colors duration-300">
-                <input
-                  type="file"
-                  accept=".jpeg,.jpg,.png,.gif,.raw"
-                  className="hidden"
-                  id="file-upload"
-                  defaultValue={`data:images/jpeg;base64, ${accountData.avatar}`}
-                />
-                <label
-                  htmlFor="file-upload"
-                  className="absolute inset-0 flex flex-col items-center justify-center cursor-pointer"
+    <>
+    {
+      loading ? (
+        <p>Loading ...</p>
+      ) : (
+        <div>
+          <Header headerTitle={Title.Account}></Header>
+          <div className="my-8 mx-4 grid grid-cols-2 bg-white dark:bg-[#363636] transition-colors duration-300 rounded-lg shadow-lg">
+            <div className="border-r-2 border-gray-200 p-4 pr-6">
+              <div className="flex justify-between items-center">
+                <h1 className="m-2 text-2xl font-bold text-black dark:text-white transition-colors duration-300">
+                  {Section1.Title}
+                </h1>
+                <NavLink
+                  className="flex items-center gap-2 rounded-lg bg-green-500 px-4 py-2 font-bold text-white"
+                  to="my-account-edit-page"
                 >
-                  <img alt="Camera Icon" className="w-9 h-9 mb-4" />
-
-                  <span className="text-blue-500 hover:text-blue-700 text-base max-w-24 text-center">
-                    {UploadImage}
-                  </span>
-                </label>
+                  <p className="hidden sm:hidden md:hidden lg:inline-block">
+                    {Edit}
+                  </p>
+                  <img src={EditIcon} alt="Icon chỉnh sửa" />
+                </NavLink>
+              </div>
+              <div className="mx-2 my-8 flex items-center">
+                <h3 className="w-1/4 text-xl font-semibold text-black dark:text-white transition-colors duration-300">
+                  {Section1.Label1}:
+                </h3>
+                <div className="relative w-3/4 flex justify-center">
+                  <div className="w-32 h-32 rounded-full bg-gray-300 dark:bg-gray-600 flex items-center justify-center transition-colors duration-300">                                        
+                    <img 
+                      alt="Ảnh đại diện" 
+                      src={`data:images/jpeg;base64, ${accountData.avatar}`} 
+                      className="w-full h-full rounded-lg" 
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="mx-2 my-8 flex text-black dark:text-white transition-colors duration-300 items-center">
+                <h3 className="w-1/3 text-xl font-semibold justify-center">
+                  {Section1.Label2}:
+                </h3>
+                <p className="w-2/3 italic justify-start">
+                  {accountData.info.hoten}
+                </p>
+              </div>
+              <div className="mx-2 my-8 flex text-black dark:text-white transition-colors duration-300 items-center">
+                <h3 className="w-1/3 text-xl font-semibold justify-center">
+                  {Section1.Label3}:
+                </h3>
+                <p className="w-2/3 italic justify-start">
+                  {accountData.info.ngaysinh}
+                </p>
+              </div>
+              <div className="mx-2 my-8 flex text-black dark:text-white transition-colors duration-300 items-center">
+                <h3 className="w-1/3 text-xl font-semibold justify-center">
+                  {Section1.Label4}:
+                </h3>
+                <p className="w-2/3 italic justify-start">
+                  {accountData.info.sodienthoai}
+                </p>
+              </div>
+              <div className="mx-2 my-8 flex text-black dark:text-white transition-colors duration-300 items-center">
+                <h3 className="w-1/3 text-xl font-semibold justify-center">
+                  {Section1.Label5}:
+                </h3>
+                <p className="w-2/3 italic justify-start">
+                  {accountData.info.email}
+                </p>
+              </div>
+              <div className="mx-2 my-8 flex text-black dark:text-white transition-colors duration-300 items-center">
+                <h3 className="w-1/3 text-xl font-semibold justify-center">
+                  {Section1.Label6}:
+                </h3>
+                <p className="w-2/3 italic justify-start">
+                  {accountData.info.diachi}
+                </p>
+              </div>
+            </div>
+            <div className="p-4 pl-6">
+              <div className="flex justify-between">
+                <h1 className="m-2 text-2xl font-bold text-black dark:text-white transition-colors duration-300">
+                  {Section2.Title}
+                </h1>
+                <NavLink
+                  className="flex items-center gap-2 rounded-lg bg-green-500 px-4 py-2 font-bold text-white hidden"
+                  to="#"
+                >
+                  <p className="hidden sm:hidden md:hidden lg:inline-block">
+                    {Edit}
+                  </p>
+                  <img src={EditIcon} alt="Icon chỉnh sửa" />
+                </NavLink>
+              </div>
+              <div className="mx-2 my-8 flex text-black dark:text-white transition-colors duration-300 items-center">
+                <h3 className="w-1/3 text-xl font-semibold justify-center">
+                  {Section2.Label1}:
+                </h3>
+                <p className="w-2/3 italic justify-start">
+                  {accountData.info.tenchucvu}
+                </p>
+              </div>
+              <div className="mx-2 my-8 flex text-black dark:text-white transition-colors duration-300 items-center">
+                <h3 className="w-1/3 text-xl font-semibold justify-center">
+                  {Section2.Label2}:
+                </h3>
+                <p className="w-2/3 italic justify-start">
+                  {accountData.info.capdo}
+                </p>
+              </div>
+              <div className="mx-2 my-8 flex text-black dark:text-white transition-colors duration-300 items-center">
+                <h3 className="w-1/3 text-xl font-semibold justify-center">
+                  {Section2.Label3}:
+                </h3>
+                <p className="w-2/3 italic justify-start">
+                  {accountData.info.luong}
+                </p>
+              </div>
+              <div className="mx-2 my-8 flex text-black dark:text-white transition-colors duration-300 items-center">
+                <h3 className="w-1/3 text-xl font-semibold justify-center">
+                  {Section2.Label4}:
+                </h3>
+                <p className="w-2/3 italic justify-start">
+                  {accountData.info.ngaybatdau}
+                </p>
+              </div>
+              <div className="mx-2 my-8 flex text-black dark:text-white transition-colors duration-300 items-center">
+                <h3 className="w-1/3 text-xl font-semibold justify-center">
+                  {Section2.Label5}:
+                </h3>
+                <p className="w-2/3 italic justify-start">
+                  {accountData.info.thoihan}
+                </p>
               </div>
             </div>
           </div>
-          <div className="mx-2 my-8 flex text-black dark:text-white transition-colors duration-300 items-center">
-            <h3 className="w-1/3 text-xl font-semibold justify-center">
-              {Section1.Label2}:
-            </h3>
-            <p className="w-2/3 italic justify-start">
-              {accountData.info.hoten}
-            </p>
-          </div>
-          <div className="mx-2 my-8 flex text-black dark:text-white transition-colors duration-300 items-center">
-            <h3 className="w-1/3 text-xl font-semibold justify-center">
-              {Section1.Label3}:
-            </h3>
-            <p className="w-2/3 italic justify-start">
-              {accountData.info.ngaysinh}
-            </p>
-          </div>
-          <div className="mx-2 my-8 flex text-black dark:text-white transition-colors duration-300 items-center">
-            <h3 className="w-1/3 text-xl font-semibold justify-center">
-              {Section1.Label4}:
-            </h3>
-            <p className="w-2/3 italic justify-start">
-              {accountData.info.sodienthoai}
-            </p>
-          </div>
-          <div className="mx-2 my-8 flex text-black dark:text-white transition-colors duration-300 items-center">
-            <h3 className="w-1/3 text-xl font-semibold justify-center">
-              {Section1.Label5}:
-            </h3>
-            <p className="w-2/3 italic justify-start">
-              {accountData.info.email}
-            </p>
-          </div>
-          <div className="mx-2 my-8 flex text-black dark:text-white transition-colors duration-300 items-center">
-            <h3 className="w-1/3 text-xl font-semibold justify-center">
-              {Section1.Label6}:
-            </h3>
-            <p className="w-2/3 italic justify-start">
-              {accountData.info.diachi}
-            </p>
-          </div>
         </div>
-        <div className="p-4 pl-6">
-          <div className="flex justify-between">
-            <h1 className="m-2 text-2xl font-bold text-black dark:text-white transition-colors duration-300">
-              {Section2.Title}
-            </h1>
-            <NavLink
-              className="flex items-center gap-2 rounded-lg bg-green-500 px-4 py-2 font-bold text-white"
-              to="#"
-            >
-              <p className="hidden sm:hidden md:hidden lg:inline-block">
-                {Edit}
-              </p>
-              <img src={EditIcon} alt="Icon chỉnh sửa" />
-            </NavLink>
-          </div>
-          <div className="mx-2 my-8 flex text-black dark:text-white transition-colors duration-300 items-center">
-            <h3 className="w-1/3 text-xl font-semibold justify-center">
-              {Section2.Label1}:
-            </h3>
-            <p className="w-2/3 italic justify-start">
-              {accountData.info.tenchucvu}
-            </p>
-          </div>
-          <div className="mx-2 my-8 flex text-black dark:text-white transition-colors duration-300 items-center">
-            <h3 className="w-1/3 text-xl font-semibold justify-center">
-              {Section2.Label2}:
-            </h3>
-            <p className="w-2/3 italic justify-start">
-              {accountData.info.capdo}
-            </p>
-          </div>
-          <div className="mx-2 my-8 flex text-black dark:text-white transition-colors duration-300 items-center">
-            <h3 className="w-1/3 text-xl font-semibold justify-center">
-              {Section2.Label3}:
-            </h3>
-            <p className="w-2/3 italic justify-start">
-              {accountData.info.luong}
-            </p>
-          </div>
-          <div className="mx-2 my-8 flex text-black dark:text-white transition-colors duration-300 items-center">
-            <h3 className="w-1/3 text-xl font-semibold justify-center">
-              {Section2.Label4}:
-            </h3>
-            <p className="w-2/3 italic justify-start">
-              {accountData.info.ngaybatdau}
-            </p>
-          </div>
-          <div className="mx-2 my-8 flex text-black dark:text-white transition-colors duration-300 items-center">
-            <h3 className="w-1/3 text-xl font-semibold justify-center">
-              {Section2.Label5}:
-            </h3>
-            <p className="w-2/3 italic justify-start">
-              {accountData.info.thoihan}
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
+      )
+    }
+    </>
   );
 }
 export default MyAccount;
--- frontend/src/pages/MyAccount/MyAccountEditPage.jsx
+++ frontend/src/pages/MyAccount/MyAccountEditPage.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 
 // Import Context Here
@@ -16,9 +16,11 @@ import Header from "../../components/Header";
 import GoBackIcon from "../../images/icons/button/GoBack.svg";
 import GoBackDarkIcon from "../../images/icons/button/GoBack_Dark.svg";
 import SaveIcon from "../../images/icons/button/Save.svg";
+import { updateAccount } from "../../assets/MyAccount/MyACcountData";
 
 const MyAccountEditPage = () => {
   // Variable here
+  const navigate = useNavigate();
   // // For Theme Mode
   const { theme } = useTheme();
   // // For Multi-Language
@@ -27,20 +29,30 @@ const MyAccountEditPage = () => {
   const { Section1 } = t("AccountPage");
   const { UploadImage, Save } = t("Buttons");
   // // For editing my account
-  const pass = "No string";
   const { userInfo } = useAuth();
-  const [currentAccount, setCurrentAccount] = useState({});
-
+  const [currentAccount, setCurrentAccount] = useState({});  
+  const [loading, setLoading] = useState(true);
+  // Variables for image here
+  const [imageForShow, setImageForShow] = useState();
+  const [imageForUpload, setImageForUpload] = useState();
+  const [userName, setUserName] = useState("");
+  const [date, setDate] = useState();
+  const [address, setAddress] = useState("");
   // Use Effect here
   // // For getting current loged account infomation
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const accoutInfo = await getStaffDetail(userInfo.staffID);
-        if (accoutInfo.length === 0) {
-          setCurrentAccount([]);
+        const accountInfo = await getStaffDetail(userInfo.userID);        
+        if (accountInfo.length === 0) {
+          setCurrentAccount([]);          
         } else {
-          setCurrentAccount(accoutInfo);
+          setLoading(false);
+          setCurrentAccount(accountInfo);
+          setUserName(accountInfo.info.hoten);
+          setDate(accountInfo.info.ngaysinh)
+          setAddress(accountInfo.info.diachi);
+          setImageForShow(accountInfo.avatar);
         }
       } catch (error) {
         console.error("Error while fetching: ", error);
@@ -48,115 +60,180 @@ const MyAccountEditPage = () => {
     };
     fetchData();
   }, []);
+  // Function for uploading new image
+  const handleUploadImage = (e) => {
+    setImageForUpload(e.target.files[0]);
+    setImageForShow(URL.createObjectURL(e.target.files[0]));
+  };
+  // Function for upadting
+  const handleUpdate = async(hoten, ngaysinh, sodienthoai, email, diachi, hinhanh) => {
+    let check_hoten = true;
+    let check_ngaysinh = true;
+    let check_sodienthoai = true;
+    let check_email = true;
+    let check_diachi = true;
+    let check_hinhanh = true;
 
+    if (check_hoten && check_ngaysinh && check_sodienthoai && check_email && check_diachi && check_hinhanh) {
+      let item = {
+        "hinhanh": hinhanh,
+        "hoten": hoten,
+        "ngaysinh": ngaysinh,
+        "sodienthoai": sodienthoai,
+        "email": email,
+        "diachi": diachi
+      }
+      const response = await updateAccount(userInfo.userID, item);
+      console.log(response);
+      if (response.success == true) {
+        alert(response.message);
+        navigate("/my-account")
+      } else {
+        alert(response.message);
+      }
+    };
+  };
   return (
-    <div>
-      <Header></Header>
-      <div className="flex items-center gap-40 mx-8 my-4">
-        <NavLink to={"/my-account"}>
-          <button>
-            <img
-              src={theme === "light" ? GoBackIcon : GoBackDarkIcon}
-              alt="Icon trở lại"
-              className="h-12 w-12"
-            />
-          </button>
-        </NavLink>
-      </div>
-      <div className="mx-8 my-4 p-4 bg-white dark:bg-[#363636] transition-colors duration-300 rounded-lg shadow-lg">
-        <h1 className="p-2 text-3xl font-bold text-black dark:text-white transition-colors duration-300">
-          {EP_Account.Title}
-        </h1>
-        <div className="flex p-4 items-center">
-          <h3 className="w-1/6 text-xl font-semibold text-black dark:text-white transition-colors duration-300">
-            {Section1.Label1}:
-          </h3>
-          <div className="relative w-5/6 flex justify-center">
-            <div className="w-36 h-36 rounded-full bg-gray-300 dark:bg-gray-600 flex items-center justify-center transition-colors duration-300">
+    <>
+    {
+      loading ? (
+        <p>Loading ...</p>
+      ) : (
+        <div>
+          <Header></Header>
+          <div className="flex items-center gap-40 mx-8 my-4">
+            <NavLink to={"/my-account"}>
+              <button>
+                <img
+                  src={theme === "light" ? GoBackIcon : GoBackDarkIcon}
+                  alt="Icon trở lại"
+                  className="h-12 w-12"
+                />
+              </button>
+            </NavLink>
+          </div>
+          <div className="mx-8 my-4 p-4 bg-white dark:bg-[#363636] transition-colors duration-300 rounded-lg shadow-lg">
+            <h1 className="p-2 text-3xl font-bold text-black dark:text-white transition-colors duration-300">
+              {EP_Account.Title}
+            </h1>
+            <div className="flex p-4 items-center">
+              <h3 className="w-1/6 text-xl font-semibold text-black dark:text-white transition-colors duration-300">
+                {Section1.Label1}:
+              </h3>
+              <div className="w-5/6 flex flex-col justify-center">
+                <div className="flex justify-center">
+                  {
+                    imageForUpload === undefined ? (
+                      <img
+                        alt="Ảnh đại diện"
+                        src={`data:image/jpeg;base64, ${imageForShow}`}
+                      ></img>
+                    ) : (
+                      <img
+                        alt="Hình đại diện"
+                        src={imageForShow}
+                      ></img>
+                    )
+                  }
+                </div>
+                <div className="rounded-full flex items-center justify-center transition-colors duration-300 my-10">
+                  <input
+                    type="file"
+                    accept=".jpeg,.jpg,.png,.gif,.raw"
+                    className="hidden"
+                    id="file-upload"
+                    onChange={handleUploadImage}
+                  />
+                  <label
+                    htmlFor="file-upload"
+                    className="flex flex-col items-center justify-center cursor-pointer"
+                  >
+                    <span className="text-blue-400 font-bold hover:text-blue-600 text-base max-w-24 text-center">
+                      {UploadImage}
+                    </span>
+                  </label>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col p-4 gap-2 text-black dark:text-white transition-colors duration-300">
+              <h3 className="h-1/4 text-xl font-semibold justify-center">
+                {Section1.Label2}:
+              </h3>
               <input
-                type="file"
-                accept=".jpeg,.jpg,.png,.gif,.raw"
-                className="hidden"
-                id="file-upload"
-                defaultValue={`data:images/jpeg;base64, ${currentAccount.avatar}`}
+                className="h-3/4 p-2 border border-black bg-white rounded-md dark:border-white dark:bg-[#363636]"
+                type="text"
+                value={userName}
+                onChange={(e) => setUserName(e.target.value)}
               />
-              <label
-                htmlFor="file-upload"
-                className="absolute inset-0 flex flex-col items-center justify-center cursor-pointer"
-              >
-                <img alt="Camera Icon" className="w-9 h-9 mb-4" />
-
-                <span className="text-blue-500 hover:text-blue-700 text-base max-w-24 text-center">
-                  {UploadImage}
-                </span>
-              </label>
+            </div>
+            <div className="flex flex-col p-4 gap-2 text-black dark:text-white transition-colors duration-300">
+              <h3 className="h-1/4 text-xl font-semibold justify-center">
+                {Section1.Label3}:
+              </h3>
+              <input
+                className="h-3/4 p-2 border border-black bg-white rounded-md dark:border-white dark:bg-[#363636]"
+                type="date"
+                value={date}
+                onChange={(e) => setDate(e.target.value)}
+              />
+            </div>
+            <div className="flex flex-col p-4 gap-2 text-black dark:text-white transition-colors duration-300">
+              <h3 className="h-1/4 text-xl font-semibold justify-center">
+                {Section1.Label4}:
+              </h3>
+              <input
+                className="h-3/4 p-2 border border-black bg-white rounded-md dark:border-white dark:bg-[#363636]"
+                type="text"
+                value={currentAccount.info.sodienthoai}
+                disabled="true"
+              />
+            </div>
+            <div className="flex flex-col p-4 gap-2 text-black dark:text-white transition-colors duration-300">
+              <h3 className="h-1/4 text-xl font-semibold justify-center">
+                {Section1.Label5}:
+              </h3>
+              <input
+                className="h-3/4 p-2 border border-black bg-white rounded-md dark:border-white dark:bg-[#363636]"
+                type="text"
+                value={currentAccount.info.email}
+                disabled="true"
+              />
+            </div>
+            <div className="flex flex-col p-4 gap-2 text-black dark:text-white transition-colors duration-300">
+              <h3 className="h-1/4 text-xl font-semibold justify-center">
+                {Section1.Label6}:
+              </h3>
+              <input
+                className="h-3/4 p-2 border border-black bg-white rounded-md dark:border-white dark:bg-[#363636]"
+                type="text"
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+              />
             </div>
           </div>
-        </div>
-        <div className="flex flex-col p-4 gap-2 text-black dark:text-white transition-colors duration-300">
-          <h3 className="h-1/4 text-xl font-semibold justify-center">
-            {Section1.Label2}:
-          </h3>
-          <input
-            className="h-3/4 p-2 border border-black bg-white rounded-md dark:border-white dark:bg-[#363636]"
-            type="text"
-            defaultValue={currentAccount.info.hoten}
-          />
-        </div>
-        <div className="flex flex-col p-4 gap-2 text-black dark:text-white transition-colors duration-300">
-          <h3 className="h-1/4 text-xl font-semibold justify-center">
-            {Section1.Label3}:
-          </h3>
-          <input
-            className="h-3/4 p-2 border border-black bg-white rounded-md dark:border-white dark:bg-[#363636]"
-            type="date"
-            defaultValue={currentAccount.info.ngaysinh}
-          />
-        </div>
-        <div className="flex flex-col p-4 gap-2 text-black dark:text-white transition-colors duration-300">
-          <h3 className="h-1/4 text-xl font-semibold justify-center">
-            {Section1.Label4}:
-          </h3>
-          <input
-            className="h-3/4 p-2 border border-black bg-white rounded-md dark:border-white dark:bg-[#363636]"
-            type="text"
-            defaultValue={currentAccount.info.sodienthoai}
-            disabled="true"
-          />
-        </div>
-        <div className="flex flex-col p-4 gap-2 text-black dark:text-white transition-colors duration-300">
-          <h3 className="h-1/4 text-xl font-semibold justify-center">
-            {Section1.Label5}:
-          </h3>
-          <input
-            className="h-3/4 p-2 border border-black bg-white rounded-md dark:border-white dark:bg-[#363636]"
-            type="text"
-            defaultValue={currentAccount.info.email}
-            disabled="true"
-          />
-        </div>
-        <div className="flex flex-col p-4 gap-2 text-black dark:text-white transition-colors duration-300">
-          <h3 className="h-1/4 text-xl font-semibold justify-center">
-            {Section1.Label6}:
-          </h3>
-          <input
-            className="h-3/4 p-2 border border-black bg-white rounded-md dark:border-white dark:bg-[#363636]"
-            type="text"
-            defaultValue={currentAccount.info.diachi}
-          />
-        </div>
-      </div>
-      <div className="w-full mx-8 my-4 flex items-center justify-center">
-        <button>
-          <div className="flex gap-2 bg-red-500 p-2 rounded-lg items-center m-2">
-            <img src={SaveIcon} alt="Icon lưu" />
-            <p className="font-bold text-white hidden sm:hidden md:hidden lg:inline-block">
-              {Save}
-            </p>
+          <div className="w-full mx-8 my-4 flex items-center justify-center">
+            <button
+              onClick={() => {handleUpdate(
+                userName,
+                date,
+                currentAccount.info.sodienthoai,
+                currentAccount.info.email,
+                address,
+                imageForUpload
+              );}}
+            >
+              <div className="flex gap-2 bg-red-500 p-2 rounded-lg items-center m-2">
+                <img src={SaveIcon} alt="Icon lưu" />
+                <p className="font-bold text-white hidden sm:hidden md:hidden lg:inline-block">
+                  {Save}
+                </p>
+              </div>
+            </button>
           </div>
-        </button>
-      </div>
-    </div>
+        </div>
+      )
+    }
+    </>
   );
 };
 
