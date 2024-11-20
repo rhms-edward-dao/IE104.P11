9479fa5779cebf4b86585457115b6d9b3459e10c
rhms-edward-dao <edwarddao20@gmail.com>
2024-11-20 23:56:29 +0700

    Update Fetching Store Data for Store Management Page and Store Management Edit Page

--- frontend/src/pages/StoreManagement/StoreManagement.jsx
+++ frontend/src/pages/StoreManagement/StoreManagement.jsx
@@ -29,12 +29,12 @@ function StoreManagement() {
   const { StoreManagementP } = t("StoreManagementP");
   const { Edit, Maintain } = t("Buttons");
   // // For fetching Data
-  const [storeData, setStoreData] = useState({});  
+  const [storeData, setStoreData] = useState({});
   const [loading, setLoading] = useState(true);
   // Functions here
   // useEffect for getting data for the first time you access this page
   useEffect(() => {
-    const fetchData = async(id) => {
+    const fetchData = async (id) => {
       const existedData = await getStoreById(id);
       setStoreData(existedData);
       setLoading(false);
@@ -44,93 +44,101 @@ function StoreManagement() {
   // Return render here
   return (
     <>
-      {
-        loading ? (
-          <p>Loading ...</p>
-        ) : (
+      {loading ? (
+        <p>Loading ...</p>
+      ) : (
+        <div>
           <div>
-            <div>
-              <Header headerTitle={StoreManagement}></Header>
-            </div>
-            <div className="my-8 mx-4 bg-white dark:bg-[#363636] transition-colors duration-300 rounded-lg shadow-lg">
-              <div className="p-4 pr-6">
-                <div className="flex justify-between">
-                  <h1 className="m-2 text-2xl font-bold text-black dark:text-white transition-colors duration-300">
-                    Thông tin đại lý
-                  </h1>
+            <Header headerTitle={StoreManagement}></Header>
+          </div>
+          <div className="my-8 mx-4 bg-white dark:bg-[#363636] transition-colors duration-300 rounded-lg shadow-lg">
+            <div className="p-4 pr-6">
+              <div className="flex justify-between">
+                <h1 className="m-2 text-2xl font-bold text-black dark:text-white transition-colors duration-300">
+                  Thông tin đại lý
+                </h1>
+                <NavLink
+                  className="flex items-center gap-2 rounded-lg bg-green-500 px-4 py-2 font-bold text-white"
+                  to="store-management-edit-page"
+                >
+                  <p className="hidden sm:hidden md:hidden lg:inline-block">
+                    {Edit}
+                  </p>
+                  <img src={EditIcon} alt="Icon chỉnh sửa" />
+                </NavLink>
+              </div>
+              <div className="mx-2 my-8 flex items-center justify-center">
+                <h3 className="w-1/4 text-xl font-semibold text-black dark:text-white transition-colors duration-300">
+                  Ảnh đại lý:
+                </h3>
+                <div className="w-1/4 flex justify-start">
+                  <div className="w-32 h-32 rounded-full bg-gray-300 dark:bg-gray-600 flex items-center justify-center transition-colors duration-300">
+                    <img
+                      alt="Ảnh đại diện"
+                      src={`data:image/jpeg;base64, ${storeData[0].hinhanh}`}
+                      className="w-full h-full rounded-full"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="mx-2 my-8 flex justify-center text-black dark:text-white transition-colors duration-300">
+                <h3 className="w-1/4 text-xl font-semibold justify-center">
+                  Tên đại lý:
+                </h3>
+                <p className="w-1/4 italic justify-start">
+                  {storeData[0].tendaily}
+                </p>
+              </div>
+              <div className="mx-2 my-8 flex justify-center text-black dark:text-white transition-colors duration-300">
+                <h3 className="w-1/4 text-xl font-semibold justify-center">
+                  Loại đại lý:
+                </h3>
+                <p className="w-1/4 italic justify-start">
+                  {storeData[0].tenloaidaily}
+                </p>
+              </div>
+              <div className="mx-2 my-8 flex justify-center text-black dark:text-white transition-colors duration-300">
+                <h3 className="w-1/4 text-xl font-semibold justify-center">
+                  Ngày tiếp nhận:
+                </h3>
+                <p className="w-1/4 italic justify-start">
+                  {storeData[0].ngaytiepnhan}
+                </p>
+              </div>
+              <div className="mx-2 my-8 flex justify-center text-black dark:text-white transition-colors duration-300">
+                <h3 className="w-1/4 text-xl font-semibold justify-center">
+                  Số điện thoại:
+                </h3>
+                <p className="w-1/4 italic justify-start">
+                  {storeData[0].sodienthoai}
+                </p>
+              </div>
+              <div className="mx-2 my-8 flex justify-center text-black dark:text-white transition-colors duration-300">
+                <h3 className="w-1/4 text-xl font-semibold justify-center">
+                  Địa chỉ
+                </h3>
+                <p className="w-1/4 italic justify-start">
+                  {storeData[0].diachi}
+                </p>
+              </div>
+              <div className="my-8 flex justify-center">
+                <div className="w-1/2 flex">
                   <NavLink
-                    className="hidden flex items-center gap-2 rounded-lg bg-green-500 px-4 py-2 font-bold text-white"
-                    to="store-management-edit-page"
+                    className="flex items-center gap-2 rounded-lg bg-green-500 px-4 py-2 font-bold text-white"
+                    to={`/store-maintainance/${userInfo.storeID}`}
                   >
                     <p className="hidden sm:hidden md:hidden lg:inline-block">
-                      {Edit}
+                      {Maintain}
                     </p>
-                    <img src={EditIcon} alt="Icon chỉnh sửa" />
+                    <img src={MaintainIcon} alt="Icon bảo trì" />
                   </NavLink>
-                </div>
-                <div className="mx-2 my-8 flex items-center justify-center">
-                  <h3 className="w-1/4 text-xl font-semibold text-black dark:text-white transition-colors duration-300">
-                    Ảnh đại lý:
-                  </h3>
-                  <div className="w-1/4 flex justify-start">
-                    <div className="w-32 h-32 rounded-full bg-gray-300 dark:bg-gray-600 flex items-center justify-center transition-colors duration-300">                                            
-                      <img 
-                        alt="Ảnh đại diện" 
-                        src={`data:image/jpeg;base64, ${storeData.hinhanh}`}
-                        className="w-full h-full rounded-full" 
-                      />
-                    </div>
-                  </div>
-                </div>
-                <div className="mx-2 my-8 flex justify-center text-black dark:text-white transition-colors duration-300">
-                  <h3 className="w-1/4 text-xl font-semibold justify-center">
-                    Tên đại lý:
-                  </h3>
-                  <p className="w-1/4 italic justify-start">{storeData.Daily[0].tendaily}</p>
-                </div>
-                <div className="mx-2 my-8 flex justify-center text-black dark:text-white transition-colors duration-300">
-                  <h3 className="w-1/4 text-xl font-semibold justify-center">
-                    Loại đại lý:
-                  </h3>
-                  <p className="w-1/4 italic justify-start">{storeData.Daily[0].tenloaidaily}</p>
-                </div>
-                <div className="mx-2 my-8 flex justify-center text-black dark:text-white transition-colors duration-300">
-                  <h3 className="w-1/4 text-xl font-semibold justify-center">
-                    Ngày tiếp nhận:
-                  </h3>
-                  <p className="w-1/4 italic justify-start">{storeData.Daily[0].ngaytiepnhan}</p>
-                </div>
-                <div className="mx-2 my-8 flex justify-center text-black dark:text-white transition-colors duration-300">
-                  <h3 className="w-1/4 text-xl font-semibold justify-center">
-                    Số điện thoại:
-                  </h3>
-                  <p className="w-1/4 italic justify-start">{storeData.Daily[0].sodienthoai}</p>
-                </div>
-                <div className="mx-2 my-8 flex justify-center text-black dark:text-white transition-colors duration-300">
-                  <h3 className="w-1/4 text-xl font-semibold justify-center">
-                    Địa chỉ
-                  </h3>
-                  <p className="w-1/4 italic justify-start">{storeData.Daily[0].diachi}</p>
-                </div>
-                <div className="my-8 flex justify-center">                  
-                  <div className="w-1/2 flex">
-                    <NavLink
-                      className="flex items-center gap-2 rounded-lg bg-green-500 px-4 py-2 font-bold text-white"
-                      to={`/store-maintainance/${userInfo.storeID}`}
-                    >
-                      <p className="hidden sm:hidden md:hidden lg:inline-block">
-                        {Maintain}
-                      </p>
-                      <img src={MaintainIcon} alt="Icon bảo trì" />
-                    </NavLink>
-                  </div>
                 </div>
               </div>
             </div>
           </div>
-        )
-      }
-    </>    
+        </div>
+      )}
+    </>
   );
 }
 export default StoreManagement;
